126b9f2a31ed111746880d63e16ee6d82502f024
benj-lazaro <benjie.work@gmail.com>
2024-12-02 22:59:58 +0800

    03 the any type

---
+++ 02-fundamentals/03-the-any-type/code/src/index.ts
@@ -0,0 +1,9 @@
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+
+// Implicitly assigning an "any" type to a variable
+// Variable loses the ability to perform a type check
+let level;
+level = 1;
+level = "one";
